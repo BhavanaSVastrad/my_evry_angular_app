51534986a070cb7cd972ff00c39f7cf85210a340
BhavanaSVastrad <bhavanasvastrad@gmail.com>
2022-09-15 21:41:02 +0530

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SigninComponent } from './signin/signin.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { SignupComponent } from './signup/signup.component';
 
-const routes: Routes = [];
+import { ContactusComponent } from './contactus/contactus.component';
+const routes: Routes = [
+  {path:'',component:CarouselComponent},
+  
+  {path:'signin',component:SigninComponent},
+  {path:'signup',component:SignupComponent},
+  {path:'contactus',component:ContactusComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
