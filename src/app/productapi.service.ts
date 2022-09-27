be2959534fd2f922065896ce4a89907c4f20fe8c
BhavanaSVastrad <bhavanasvastrad@gmail.com>
2022-09-27 21:43:56 +0530

    commit

--- src/app/productapi.service.ts
+++ src/app/productapi.service.ts
@@ -1,16 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map } from 'rxjs/operators';
+//postsapi - heroku
 import { environment } from 'src/environments/environment';
-//json-server --watch db.json
+//json-server --watch db.json for localhost
 //npm install -g json-server
 @Injectable({
   providedIn: 'root'
 })
 export class ProductapiService {
-  
   constructor(private http:HttpClient) { }
-  posturl=environment.postapi;
+posturl=environment.postapi;
   postProducts(data:any){
     return this.http.post<any>(this.posturl, data).pipe(map((res:any)=>{
       return res;
@@ -24,13 +24,13 @@ export class ProductapiService {
   }
 
   updateProducts(data:any, id:number){
-    return this.http.put<any>(this.posturl+id, data).pipe(map((res:any)=>{
+    return this.http.put<any>(this.posturl+'/'+ id, data).pipe(map((res:any)=>{
       return res;
     }))
   }
 
   deleteProducts(id:number){
-    return this.http.delete<any>(this.posturl+id).pipe(map((res:any)=>{
+    return this.http.delete<any>(this.posturl+'/'+ id).pipe(map((res:any)=>{
       return res;
     }))
   }
