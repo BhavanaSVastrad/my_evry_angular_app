1d8baf47df8a55b8344e6756ce6a272c305bda64
BhavanaSVastrad <bhavanasvastrad@gmail.com>
2022-09-27 15:49:31 +0530

    commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,12 @@
 export const environment = {
   production: true,
-  userapi:"http://localhost:3000/users",
-  cartapi:"http://localhost:3000/cart",
-  postapi:"http://localhost:3000/posts",
-  adminapi:"http://localhost:3000/admin"
+  // userapi:"http://localhost:3000/users",
+  // cartapi:"http://localhost:3000/cart",
+  // postapi:"http://localhost:3000/posts",
+  // adminapi:"http://localhost:3000/admin"
+
+  postapi:"https://heroku-json-server-fakedbs.herokuapp.com/posts",
+  userapi:"https://heroku-json-server-fakedbs.herokuapp.com/users",
+  adminapi:"https://heroku-json-server-fakedbs.herokuapp.com/admin",
+  cartapi:"https://heroku-json-server-fakedbs.herokuapp.com/cart"
 };
